707777ce239c119b45cfc42e4e776566615ba619
ishaan513 <46614189+ishaan513@users.noreply.github.com>
2024-02-04 16:17:39 -0600

    Create NAV.cs
    Initial NAV functions

---
+++ UnityProjectBEARS/Assets/Scripts/NAV.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinate : MonoBehaviour {
+    protected:
+        double xCoordinate;
+        double yCoordinate;
+    public:
+        void setX();
+        void setY()
+        double getX();  
+        double getY();
+
+    Coordinate(){
+        xCoordinate = 0;
+        yCoordinate = 0;
+    }
+    Coordinate(double x, double y){
+        xCoordinate = x;
+        yCoordinate = y;
+    }
+
+    void setX(double x){
+        xCoordinate = x;
+    }
+    void setY(double y){
+        yCoordinate = y;
+    }
+
+    double getX(){
+        return xCoordinate;
+    }
+    double getY(){
+        return yCoordinate;
+    }
+}
+
+public class Pin : public Coordinate{
+    private string locName;
+
+    public:
+        void setName;
+        string getName;
+    
+    Pin(){
+        locName = "";
+        xCoordinate = 0;
+        yCoordinate = 0;
+    }
+    Pin(string name, double x, double y){
+        locName = name;
+        xCoordinate = x;
+        yCoordinate = y;
+    }
+
+    void setName(string name){
+        locName = name;
+    }
+
+    string getName(){
+        return name;
+    }
+}
+
+public class Route{
+
+}
+public class NAV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //public void changeFIVEVAR() :   [5 variations of same function]
+        //if variable of status is false, set gameobject to true
+        // let data rate change
+        //else (if true), set to false
+        // data won't change or will contantly rise/fall
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
